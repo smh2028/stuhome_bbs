0802a6a51906e2e059972ca6821c10fa9e8f3623
smh2028 <smh2208@163.com>
2018-07-22 17:05:12 +0800

    添加是否要水贴选项

--- stuhome_spider.py
+++ stuhome_spider.py
@@ -8,8 +8,6 @@
 '''
 
 import requests,re
-from bs4 import BeautifulSoup
-from urllib.parse import quote
 from scrapy.selector import Selector
 from time import time,sleep
 import threading
@@ -25,6 +23,7 @@ class StuhomeSpider():
     tiezi_url = 'http://bbs.uestc.edu.cn/forum.php?mod=viewthread&tid=1727470&extra=page%3D1'
     reply_url = 'http://bbs.uestc.edu.cn/forum.php?mod=post&action=reply&fid={0}&tid={1}&extra={2}&replysubmit=yes' \
                 '&infloat=yes&handlekey=fastpost&inajax=1'
+    reply_message = 'test data from pp'
     session = requests.session()
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -55,11 +54,11 @@ class StuhomeSpider():
             "answer": "",
         }
         self.session.headers.update(self.headers)
-        # response = self.session.get(self.log_page_url)
-        proxies = {
-            'http':'82.202.70.132:8080',
-            'https':'82.202.70.132:8080'
-        }
+        #代理测试
+        # proxies = {
+        #     'http':'82.202.70.132:8080',
+        #     'https':'82.202.70.132:8080'
+        # }
         response = self.session.post(self.log_in_url.format(self.loginhash),headers=self.headers,data=post_data
                                      )#,proxies=proxies
         #打印登录结果
@@ -67,12 +66,6 @@ class StuhomeSpider():
         if 'succeedmessage' in response.text:
             return True
         return False
-
-    def get_home_page(self):
-        pass
-        # 打印登录后访问首页的结果
-        # home_response = self.session.get(self.home_url)
-        # print(home_response.text)
 
     def get_tiezi_params_and_reply(self):
         '''根据帖子的url获取参数并发表回复'''
@@ -91,7 +84,7 @@ class StuhomeSpider():
     def reply(self,fid,tid,extra,formhash):
         '''回复某一个帖子'''
         post_data = {
-            'message': 'test data from pp',
+            'message': self.reply_message,
             'posttime': str(int(time())),
             'formhash': formhash,
             'usesig': '1',
@@ -102,30 +95,30 @@ class StuhomeSpider():
             print('回复成功！')
         else:
             print('回复失败！')
-        # print(r.status_code,r.text)
-        # try:
-        #     r = self.session.post(url=self.reply_url.format(fid,tid,extra),data=post_data,headeers=self.headers)
-        #     print('回复成功！')
-        # except :
-        #     print('回复失败，请重试!')
 
     def refresh(self):
         '''定时刷新主页，保持登录状态，刷在线时长，美滋滋'''
         while True:
-            self.session.get(self.home_url,headers=self.headers)
+            self.session.get(self.home_url, headers=self.headers)
             print('刷新主页成功，离升级又近了一步，嘿嘿')
-            #水大红贴，慎重打开，容易被封
+            # 水大红贴，慎重打开，容易被封
             # self.get_tiezi_params_and_reply()
             # print('大红贴水贴成功，嘿嘿')
             sleep(70)
+
 
     def run(self):
         self.username = input('请输入用户名：')
         self.password = input('请输入密码：')
         login_result = self.log_in()
         if login_result:
-            self.tiezi_url = input('登录成功!请输入要回复的帖子url：')
-            self.get_tiezi_params_and_reply()
+            willing2reply = input('登录成功！是否要水贴？ y/n:')
+            if willing2reply=='y':
+                self.tiezi_url = input('请输入要回复的帖子url：')
+                self.reply_message = input('请输入要回复的内容：')
+                self.get_tiezi_params_and_reply()
+            else:
+                print('开始刷新主页')
         refresh_thread = threading.Thread(target=self.refresh)
         refresh_thread.start()
 
@@ -133,3 +126,4 @@ class StuhomeSpider():
 if __name__ == '__main__':
     stuhome_sp = StuhomeSpider()
     stuhome_sp.run()
+
